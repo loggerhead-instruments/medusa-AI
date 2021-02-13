b4dcfc50db3724897bc1c7c99810d2ab8c6160af
David Mann <dmann@loggerhead.com>
2021-02-13 18:59:02 -0500

    pi test files

--- pi/audioProcess.py
+++ pi/audioProcess.py
@@ -3,8 +3,9 @@ import RPi.GPIO as GPIO
 import sys, os, glob, time
 import datetime
 from subprocess import call
-import pyaudio
 import wave
+
+print("audioProcess")
 
 statusPin = 24;
 statusPin2 = 26;
@@ -23,12 +24,14 @@ GPIO.output(statusPin2, 0)
 # - save detection counts
 
 # simulate processing delay
+print("Processing")
 sleep(10)
 
-file1 = open("\mnt\audio\detections.txt")
+file1 = open("\mnt\audio\detections.txt", "w")
 file1.write("w:10\n")
 file1.close()
 
+print("File written")
 
 GPIO.output(statusPin, 0)
 GPIO.output(statusPin2, 1)
