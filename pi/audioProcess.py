4b975fd4d84f26711d80fc249db426366ee20c18
David Mann <dmann@loggerhead.com>
2021-02-13 17:49:07 -0500

    simulate processing audio files

---
+++ pi/audioProcess.py
@@ -0,0 +1,40 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import sys, os, glob, time
+import datetime
+from subprocess import call
+import pyaudio
+import wave
+
+statusPin = 24;
+statusPin2 = 26;
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(statusPin, GPIO.OUT)
+GPIO.setup(statusPin2, GPIO.OUT)
+GPIO.output(statusPin, 1)
+GPIO.output(statusPin2, 0)
+
+
+# Process audio here
+# - read files
+# - process files
+# - save detection counts
+
+# simulate processing delay
+sleep(10)
+
+file1 = open("\mnt\audio\detections.txt")
+file1.write("w:10\n")
+file1.close()
+
+
+GPIO.output(statusPin, 0)
+GPIO.output(statusPin2, 1)
+
+sleep(2)
+
+# call("sudo nohup shutdown -h now", shell=True)
+exit()
+
