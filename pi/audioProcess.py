31d94d75371678d0f25f8c9e6bc32d411b478e0f
Austin Anderson <36173484+Path-A@users.noreply.github.com>
2021-11-16 10:20:52 -0500

    no file w/ shutdown

--- pi/audioProcess.py
+++ pi/audioProcess.py
@@ -1,22 +1,20 @@
-from time import sleep
+import time
 import RPi.GPIO as GPIO
-import sys, os, glob, time
-import datetime
+import os
 from subprocess import call
-import wave
 
 import librosa as lr
 import math
 import numpy as np
 import os
 from pathlib import Path
-import sounddevice as sd
 import soundfile as sf
 import tflite_runtime.interpreter as tflite
-import time
 
 cfg = {'inwav_dir': '/mnt/audio',
+#'inwav_dir': '/home/pi/medusa/1hour',
 'outwav_dir':'/mnt/audio/outwavs',
+#'outwav_dir':'/home/pi/medusa/outwavs',
 'save_specs': True,
 'MODEL_PATH': '/home/pi/medusa/model/mnv2-singlegpu-epoch200-128batchsmallspec_postquant_float32inout_edgetpu_16.tflite'}
 
@@ -32,11 +30,6 @@ if __name__ == "__main__":
 
     statusPin = 24
     statusPin2 = 26
-
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(statusPin, GPIO.OUT)
-    GPIO.setup(statusPin2, GPIO.OUT)
     GPIO.output(statusPin, 1)
     GPIO.output(statusPin2, 0)
 
@@ -50,75 +43,86 @@ if __name__ == "__main__":
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    with os.scandir(cfg['inwav_dir']) as directory:
-        for file in directory:
-            if file.is_file() and file.name.endswith('.wav'):
-                print("Loading Audio File: " + file.name)
-            
-                # Check if file has content
-                if os.path.getsize(file.path):
-                    data, samplerate = sf.read(file, dtype='float32')
+    num_wavs = len([wav for wav in os.listdir('/home/pi/medusa') if wav.endswith('.wav')])
 
-                    first_minute_path = Path(cfg['outwav_dir']+'/'+file.name[:-4]+'~minute'+'.wav')
-                    with sf.SoundFile(first_minute_path, mode='x', samplerate=44100,
-                    channels=1, subtype='PCM_16') as audio_file:
-                        audio_file.write(data[:minute_length])
-            
-                    number_of_chunks = math.ceil(len(data) / float(chunk_length))
-                    my_list = [data[i * chunk_length:(i + 1) * chunk_length] for i in range(int(number_of_chunks))]
+    if num_wavs == 0:
+        time.sleep(60)
 
-                    # Break sound into 1 second chunks
-                    for i, chunk in enumerate(my_list):
+        GPIO.output(statusPin, 0)
+        GPIO.output(statusPin2, 1)
 
-                        if len(chunk) == 44100:
-                            spectrogram = lr.amplitude_to_db(np.abs(lr.stft(chunk, n_fft=256, hop_length=145)))
-                            spectrogram = spectrogram[3:, :]
+        time.sleep(2)
+        call("sudo nohup shutdown -h now", shell=True)
+        exit()
+    else:
+        with os.scandir(cfg['inwav_dir']) as directory:
+            for file in directory:
+                if file.is_file() and file.name.endswith('.wav'):
+                    print("Loading Audio File: " + file.name)
+                
+                    # Check if file has content
+                    if os.path.getsize(file.path):
+                        data, samplerate = sf.read(file, dtype='float32')
 
-                            spectrogram = np.expand_dims(spectrogram, axis=(-1,0))
+                        first_minute_path = Path(cfg['outwav_dir']+'/'+file.name[:-4]+'~minute'+'.wav')
+                        with sf.SoundFile(first_minute_path, mode='x', samplerate=44100,
+                        channels=1, subtype='PCM_16') as audio_file:
+                            audio_file.write(data[:minute_length])
+                
+                        number_of_chunks = math.ceil(len(data) / float(chunk_length))
+                        my_list = [data[i * chunk_length:(i + 1) * chunk_length] for i in range(int(number_of_chunks))]
 
-                            # Make prediction
-                            interpreter.set_tensor(input_details[0]['index'], spectrogram)
-                            interpreter.invoke()
+                        # Break sound into 1 second chunks
+                        for i, chunk in enumerate(my_list):
 
-                            tflite_results = interpreter.get_tensor(output_details[0]['index'])
+                            if len(chunk) == 44100:
+                                spectrogram = lr.amplitude_to_db(np.abs(lr.stft(chunk, n_fft=256, hop_length=145)))
+                                spectrogram = spectrogram[3:, :]
+
+                                spectrogram = np.expand_dims(spectrogram, axis=(-1,0))
+
+                                # Make prediction
+                                interpreter.set_tensor(input_details[0]['index'], spectrogram)
+                                interpreter.invoke()
+
+                                tflite_results = interpreter.get_tensor(output_details[0]['index'])
 
 
-                            if np.any(tflite_results > .5):
-                                
-                                print("DOLPHIN SPOTTED!!")
-                                whistle_counter += 1
+                                if np.any(tflite_results > .5):
+                                    
+                                    print("DOLPHIN SPOTTED!!")
+                                    whistle_counter += 1
 
-                                if cfg['save_specs']:
-                                    audio_file_path = Path(cfg['outwav_dir']+'/'+file.name[:-4]+'~'+str(i)+'~'+str(tflite_results[0,0])+'.wav')
+                                    if cfg['save_specs']:
+                                        audio_file_path = Path(cfg['outwav_dir']+'/'+file.name[:-4]+'~'+str(i)+'~'+str(tflite_results[0,0])+'.wav')
 
-                                    with sf.SoundFile(audio_file_path, mode='x', samplerate=44100,
-                                    channels=1, subtype='PCM_16') as audio_file:
-                                        audio_file.write(chunk)
-                    
-                    Path(file.path).unlink()
+                                        with sf.SoundFile(audio_file_path, mode='x', samplerate=44100,
+                                        channels=1, subtype='PCM_16') as audio_file:
+                                            audio_file.write(chunk)
                         
-                else:
-                    print("ERROR: FILE SIZE 0 BYTES. SKIPPING...")
-    print("PREDICTIONS COMPLETE")
-    print(time.time() - start_time)
+                        Path(file.path).unlink()
+                            
+                    else:
+                        print("ERROR: FILE SIZE 0 BYTES. SKIPPING...")
+        print("PREDICTIONS COMPLETE")
+        print(time.time() - start_time)
 
 
 
-    # simulate processing delay
-    #print("Processing")
-    #sleep(10)
+        # simulate processing delay
+        #print("Processing")
+        #time.sleep(10)
 
-    file1 = open("/mnt/audio/detections.txt", "w")
-    file1.write(f"w:{whistle_counter}\n")
-    file1.close()
+        file1 = open("/mnt/audio/detections.txt", "w")
+        file1.write(f"w:{whistle_counter}\n")
+        file1.close()
 
-    print("File written")
+        print("File written")
 
-    GPIO.output(statusPin, 0)
-    GPIO.output(statusPin2, 1)
+        GPIO.output(statusPin, 0)
+        GPIO.output(statusPin2, 1)
 
-    sleep(2)
+        time.sleep(2)
 
-    # call("sudo nohup shutdown -h now", shell=True)
-    exit()
-
+        call("sudo nohup shutdown -h now", shell=True)
+        exit()
